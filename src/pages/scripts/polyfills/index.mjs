c844b2fcb834145657d79c54ccd4cb93d7bc4637
hxx <hxx0215@gmail.com>
2025-11-21 03:04:25 +0000

    Revert "[proj] localize remote mjs"
    This reverts commit 16ee5a48d5e7b3383807777b33407ff9c197ba40.

--- src/pages/scripts/polyfills/index.mjs
+++
@@ -1,6 +0,0 @@
-// Local polyfill entry point
-// Replaces: https://cdn.jsdelivr.net/gh/steve02081504/js-polyfill/index.mjs
-
-await import('./core-js.mjs').catch(_ => 0)
-
-if (globalThis.document) await import('./browser.mjs').catch(_ => 0)
